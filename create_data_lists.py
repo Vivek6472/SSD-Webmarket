7c083a723bf0931999472e756aa08bf4f40b8312
noobCoder96 <42578649+Vivek6472@users.noreply.github.com>
2019-07-05 11:20:59 +0530

    Update create_data_lists.py

--- create_data_lists.py
+++ create_data_lists.py
@@ -1,6 +1,4 @@
 from utils import create_data_lists
 
 if __name__ == '__main__':
-    create_data_lists(voc07_path='/media/ssd/ssd data/VOC2007',
-                      voc12_path='/media/ssd/ssd data/VOC2012',
-                      output_folder='./')
+    create_data_lists(wb_path='/media/ssd/ssd data/VOC2007', output_folder='./')
