e433ffdbf1af8d4976233f97e9d5b491e0fd8fdd
noobCoder96 <42578649+Vivek6472@users.noreply.github.com>
2019-07-06 21:20:07 +0530

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,5 +1,5 @@
 from utils import *
-from datasets import PascalVOCDataset
+from datasets import WebMarketDataset
 from tqdm import tqdm
 from pprint import PrettyPrinter
 
@@ -7,18 +7,17 @@ from pprint import PrettyPrinter
 pp = PrettyPrinter()
 
 # Parameters
-data_folder = './'
+data_folder = './' # to be edited
 keep_difficult = True  # difficult ground truth objects must always be considered in mAP calculation, because these objects DO exist!
 batch_size = 64
 workers = 4
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-checkpoint = './BEST_checkpoint_ssd300.pth.tar'
-
+'''
 # Load model checkpoint that is to be evaluated
 checkpoint = torch.load(checkpoint)
 model = checkpoint['model']
 model = model.to(device)
-
+'''
 # Switch to eval mode
 model.eval()
 
